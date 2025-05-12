c056a44857deec1a36978664e19d4939ac92bcc2
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-05-12 15:58:45 -0500

    feat: adding Complaint register page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-const Button = ({ text, className }) => {
+const Button = ({ text, className, onClick }) => {
   return (
     <button
+      onClick={onClick} 
       className={`min-w-[220px] py-3 px-6 text-center font-bold rounded-lg shadow transition duration-300 transform hover:scale-105 hover:opacity-90 ${className}`}
     >
       {text}
     </button>
   );
 };
+
 
 export default Button;
