bf83f66599383912848aade32efb14114ec939ba
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-05-21 19:25:47 -0500

    hotfix: adding mising config

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,6 +5,7 @@ const axiosInstance = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: true, // Esto es clave para que se envíen cookies
 });
 
 export default axiosInstance;
